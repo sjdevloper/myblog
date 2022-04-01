7995c7cedc37d44bdc5c387bbff891dd010247a1
sjiang20 <sjiang086@gmail.com>
2022-04-01 13:33:19 -0700

    8

---
+++ notezipper/backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const notes = require("./data/notes");
+const dotenv = require('dotenv')
+const connectDB = require("./config/db");
+const userRoutes = require('./routes/userRoutes')
+
+const app = express();
+dotenv.config();
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("API is running..");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
